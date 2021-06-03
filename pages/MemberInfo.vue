f7b872c3d455b7e1d0ba61b99cfb7dd399329a4a
VeronicaLucchetti <83123387+VeronicaLucchetti@users.noreply.github.com>
2021-06-03 21:50:01 +0200

    Added page for detailed information of members

---
+++ pages/MemberInfo.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="root">
+        <Navbar />
+        <Breadcrumbs />
+        <mdb-container>
+            <mdb-card reverse class="my-5">
+                <Description :member="mem" mem />
+            </mdb-card>
+        </mdb-container>
+        <Footer />
+    </div>
+</template>
+
+<script>
+    import { mdbContainer, mdbCard } from 'mdbvue';
+
+    export default {
+        components: {
+            mdbContainer,
+            mdbCard
+        },
+        data () {
+            return {
+                mem: this.$route.query.name.replace('_',' '),
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>

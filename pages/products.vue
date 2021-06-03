bacf0b8c69ab811ab6a33e20f46cde250665ff0b
LorenzoGrossi <83112776+LorenzoGrossi@users.noreply.github.com>
2021-06-03 22:03:25 +0200

    All the products page

---
+++ pages/products.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="root">
+        <Navbar />
+        <Breadcrumbs />
+        <mdb-container>
+            <mdb-card v-for="(area,i) in areas" :key="i" class="my-5" reverse>
+                <List :ar="area" product short needarea/>
+            </mdb-card>
+        </mdb-container>
+        <Footer />
+    </div>
+</template>
+
+<script>
+    import AreaDataService from '../services/AreaDataService'
+    import { mdbContainer, mdbCard} from 'mdbvue';
+
+    export default {
+        components: {
+            mdbCard,
+            mdbContainer
+        },
+        data () {
+            return {
+                areas : [],
+                members : [],
+                mem: []
+            }
+        },
+
+        methods: {
+            retrieveAreas() {
+                AreaDataService.getAll().then(response => {
+                    this.areas = response.data;
+                })
+                .catch(e => {
+                    (e);
+                })
+
+            }
+        },
+        mounted() {
+            this.retrieveAreas();
+        }
+    }
+</script>

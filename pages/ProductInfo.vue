baaf363f11ddc35c4f40f2a9821f297b97a4c45a
VeronicaLucchetti <83123387+VeronicaLucchetti@users.noreply.github.com>
2021-06-03 21:51:55 +0200

    Added page for detailed information of products

---
+++ pages/ProductInfo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="root">
+        <Navbar />
+        <Breadcrumbs />
+        <mdb-container>
+            <mdb-card reverse class="my-5">
+                <Description :product="prod" prod />
+            </mdb-card>
+        </mdb-container>
+        <Footer />
+    </div>
+</template>
+
+<script>
+
+    import { mdbContainer, mdbCard } from 'mdbvue';
+    import Description from '../../components/Description.vue';
+
+    export default {
+        components: {
+            mdbContainer,
+            mdbCard,
+            Description
+        },
+        data () {
+            return {
+                prod: this.$route.query.name,
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+</style>

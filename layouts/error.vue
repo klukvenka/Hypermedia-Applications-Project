227317db7d4288747a836b120d427621b718e65c
sairaghav <4383992+sairaghav@users.noreply.github.com>
2021-06-05 22:19:38 +0200

    Standard error page

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="root">
+        <Navbar />
+        <div class="error">
+            <div class="text-center">
+                <h2>Oops...</h2>
+                <h3 class="light-heading">Seems like there is some error in your request</h3>
+                <h4 class="light-heading">Go to <a href="/">home</a></h4>
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<style scoped>
+.error {
+    height: 90vh;
+    position: relative;
+    padding-top: 60px;
+    
+}
+.light-heading {
+    text-align: center;
+}
+
+</style>

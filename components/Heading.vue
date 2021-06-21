8bde3ff74b74247b2876790586aa6b9e98f04e0c
sairaghav <4383992+sairaghav@users.noreply.github.com>
2021-06-21 12:22:38 +0200

    Cosmetic changes

--- components/Heading.vue
+++ components/Heading.vue
@@ -2,6 +2,7 @@
     <div class="heading text-center">
         <h2>{{heading}}</h2>
         <h3>{{subheading}}</h3>
+        <br>
         <h5 class="light-heading">{{description}}</h5>
     </div>
     

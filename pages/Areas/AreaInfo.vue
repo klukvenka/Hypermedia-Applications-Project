1ab02766469a0692ae41c64028e483fccd0b4b48
VeronicaLucchetti <83123387+VeronicaLucchetti@users.noreply.github.com>
2021-06-23 16:49:20 +0200

    Added comments for AreaInfo

--- pages/Areas/AreaInfo.vue
+++ pages/Areas/AreaInfo.vue
@@ -1,17 +1,37 @@
+<!--
+Author: Veronica Lucchetti
+Description:
+The page displays the "single area" page of the website.
+
+The page contains
+a navigation bar on the top,
+a description of the area,
+the products contained in the area and their images,
+the people who work in the area and their pictures.
+
+It allows the user to scroll through the images of the products and the people,
+It contains transiction links to the products and the people,
+moreover it allows the user to read small descriptions about products and people clicking on the images.
+-->
+
 <template>
     <div class="root">
         <Topbar />
         <div class="content">
+        <!--Start main content--> 
+        <!--Title on top of page and description of the area below-->
             <Heading :heading="areas.name" :description="areas.description" />
         </div>
         <div class="content">
             <div>
+                <!--Display each product related to that area in a section titled "products"-->
                 <h2 class="">Products</h2>
                 <short-page :forArea="areas" getProduct needToggleInfo />
             </div>
         </div>
         <div class="content">
             <div>
+                <!--Display each people related to that area in a section titled "team members"-->
                 <h2 class="">Team Members</h2>
                 <short-page :forArea="areas" getMember needToggleInfo />
             </div>

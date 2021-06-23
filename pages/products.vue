fc7f41706bb32853eb87a74ffb06f2747b68616d
Lorenzo Grossi <Lorenzo@MacBook-Pro.fritz.box>
2021-06-23 17:10:12 +0200

    Comments for introductory pages

--- pages/products.vue
+++ pages/products.vue
@@ -1,6 +1,15 @@
+<!--
+Author: Lorenzo Grossi
+Description:
+The page displays the introductory page with the list of all the products sorted by area.
+
+After landmarks, a section with the name of a single area is showed with the list of products with name, picture and 
+a short description accessible clicking the product image.
+-->
 <template>
     <div class="root">
         <Topbar />
+        <!-- Displaying a section for each area with relative products-->
         <div v-for="(area,i) in areas" :key="i" class="content">
             <short-page :forArea="area" getProduct needHeading needToggleInfo/>
         </div>
